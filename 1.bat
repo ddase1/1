be7d28fc6694706a950c2263de01903dc2414f5a
ddase1 <865886582@qq.com>
2018-05-01 15:47:09 +0800

    1

---
+++ 1.bat
@@ -0,0 +1,6 @@
+git clone https://github.com/ddase1/1.git 
+git config --global user.email "865886582@qq.com"
+git config --global user.name "ddase1"
+git add .
+git commit  -m  "1"
+git push -u origin master  
